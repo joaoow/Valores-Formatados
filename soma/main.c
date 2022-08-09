1ed7e6ad0392e19a2011f390be5bbe314e40e268
joaolinux <joaomotionless570@gmail.com>
2022-08-09 11:30:11 -0300

    Add files via upload

---
+++ soma/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main ()
+{
+      int x = 10;
+   printf("%i\n", x );
+
+   float ff = 4.12;
+   printf("%f\n", ff);
+
+   printf("%i - %f \n", x, ff);
+
+   return 0;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
